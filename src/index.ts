7f3ab03feb214b47d0bba7a354d881c760c3b5b3
Andrea Jemmett <andreajemmett@gmail.com>
2017-10-06 13:01:46 +0200

    added topics service

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export * from './modals.service';
 export * from './modals.component';
 export * from './shared';
 export * from './skills.service';
+export * from './topics.service';
 export * from './user.resolver';
 export * from './user.service';
 export * from './utils';
