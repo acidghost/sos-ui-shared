9911f3beee5e11599fd6cd36f2e5eda1fb78ba5e
Andrea Jemmett <andreajemmett@gmail.com>
2018-01-08 14:43:13 +0100

    fablab machine reservations

--- src/fablab.models.ts
+++ src/fablab.models.ts
@@ -1,3 +1,6 @@
+import {UserShort} from "./shared";
+
+
 export interface FablabMachine {
   id: number,
   name: string,
@@ -9,4 +12,32 @@ export interface FablabMachine {
   engravesMaterials?: string,
   priceHour: number,
   operator: boolean
+}
+
+
+export interface FablabReservation {
+  id: number,
+  machineId: number,
+  user: UserShort,
+  startTime: Date,
+  endTime: Date,
+  realizationOf: string,
+  createdAt: Date
+}
+
+
+export namespace FablabReservation {
+
+  export function fromJson(json: any): FablabReservation {
+    return {
+      id: json.id,
+      machineId: json.machineId,
+      user: json.user,
+      startTime: new Date(json.startTime),
+      endTime: new Date(json.endTime),
+      realizationOf: json.realizationOf,
+      createdAt: new Date(json.createdAt)
+    };
+  }
+
 }
--- src/fablab.service.ts
+++ src/fablab.service.ts
@@ -4,7 +4,7 @@ import {AuthService} from "./auth.service";
 import {Environment} from "./shared";
 import {Http} from "@angular/http";
 import {Observable} from "rxjs/Observable";
-import {FablabMachine} from "./fablab.models";
+import {FablabMachine, FablabReservation} from "./fablab.models";
 
 
 @Injectable()
@@ -23,6 +23,45 @@ export class FablabService extends ApiService {
   public findMachine(id: number): Observable<FablabMachine> {
     return this.http.get(`${this.backendUrl}/fablab/machines/${id}`, this.options)
       .map(response => response.json())
+      .catch(e => this.catchAuth(e));
+  }
+
+
+  private getReservations(url: string): Observable<FablabReservation[]> {
+    return this.http.get(url, this.options)
+      .map(response => response.json().reservations.map(FablabReservation.fromJson))
+      .catch(e => this.catchAuth(e));
+  }
+
+  public allReservations(): Observable<FablabReservation[]> {
+    return this.getReservations(`${this.backendUrl}/fablab/reservations`);
+  }
+
+  public machineReservations(machineId: number): Observable<FablabReservation[]> {
+    return this.getReservations(`${this.backendUrl}/fablab/machines/${machineId}/reservations`);
+  }
+
+  public userReservations(): Observable<FablabReservation[]> {
+    return this.getReservations(`${this.backendUrl}/fablab/my_reservations`);
+  }
+
+  public createReservation(reservation: FablabReservation): Observable<FablabReservation> {
+    let url = `${this.backendUrl}/fablab/machines/${reservation.machineId}/reservations`;
+
+    let payload = {
+      startTime: reservation.startTime,
+      endTime: reservation.endTime,
+      realizationOf: reservation.realizationOf
+    };
+
+    return this.http.post(url, payload, this.options)
+      .map(response => FablabReservation.fromJson(response.json()))
+      .catch(e => this.catchAuth(e));
+  }
+
+  public deleteReservation(id: number): Observable<void> {
+    return this.http.delete(`${this.backendUrl}/fablab/reservations/${id}`, this.options)
+      .map(() => {})
       .catch(e => this.catchAuth(e));
   }
 
