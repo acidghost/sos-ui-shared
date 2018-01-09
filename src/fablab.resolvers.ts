868942c71743d40489db0e0e6732e7c5ca3cdbe0
Andrea Jemmett <andreajemmett@gmail.com>
2018-01-09 14:57:30 +0100

    added fablab machine reservations resolver

--- src/fablab.resolvers.ts
+++ src/fablab.resolvers.ts
@@ -1,7 +1,7 @@
 import {Injectable} from "@angular/core";
 import {ApiResolver} from "./api.resolver";
 import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
-import {FablabMachine} from "./fablab.models";
+import {FablabMachine, FablabReservation} from "./fablab.models";
 import {AuthService} from "./auth.service";
 import {FablabService} from "./fablab.service";
 import {Observable} from "rxjs/Observable";
@@ -30,6 +30,20 @@ export class FablabMachineResolver extends ApiResolver implements Resolve<Fablab
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FablabMachine> {
     return this.fablabService.findMachine(parseInt(route.paramMap.get('id')));
+  }
+
+}
+
+
+@Injectable()
+export class FablabMachineReservationsResolver extends ApiResolver implements Resolve<FablabReservation[]> {
+
+  constructor(authService: AuthService, private fablabService: FablabService) {
+    super(authService, fablabService)
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<FablabReservation[]> {
+    return this.fablabService.machineReservations(parseInt(route.paramMap.get('id')));
   }
 
 }
